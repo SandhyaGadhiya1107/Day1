da14821f0e87948dfe2a42612d0a14429f3b3ae8
SandhyaGadhiya1107 <84837955+SandhyaGadhiya1107@users.noreply.github.com>
2021-05-26 22:12:36 +0530

    Day_2

---
+++ 07_Task.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Leap Year demo</title>
+</head>
+<body>
+<?php
+$year=2020;
+print"</br>";
+if($year%4==1)
+{
+    printf("Year %d is not a leap year",$year);
+}
+else{
+    printf("Year %d is a leap year",$year);
+}
+?>
+</body>
+</html>
